cf4d19aa283a68e62e32b70b40b045bce1075baf
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-14 16:38:47 -0700

    o

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 01; i < 100; i++)
+	{putchar('00\t' '0' + i); }
+	putchar('\n');
+	return (0);
+}
