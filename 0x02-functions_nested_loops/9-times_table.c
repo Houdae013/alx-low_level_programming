b6b837f0203e5bdf9557476266389fcffb307c15
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-18 17:07:45 -0700

    o

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,16 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
+			if (i == 0)
+			{_putchar('0'); }
+			else
+			{
 			if (((i * j) / 10) == 0)
 			{_putchar (j + '0'); }
 			else
 			{_putchar (((i * j) / 10) + '0');
 				_putchar (((i * j) % 10) + '0');
-			}
+			} }
 			_putchar (',');
 			_putchar(32);
 		}
