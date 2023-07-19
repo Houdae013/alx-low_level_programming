2a7e251d74b54a8d545f8a69b66d4f3c1d01fdc2
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-19 15:42:27 -0700

    j:wq

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_times_table - checks for lower
+ *@n: int
+ * On error: return 0
+ * Return: 1 if success
+ */
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	if (n <= 15 && n > 0)
+	{
+		for (a = 0; a <= n; a++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (b = 1; b <= n; b++)
+		{
+			c = (a * b);
+			if ((c / 10) > 0)
+			{
+				_putchar((c / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((c % 10) + '0');
+
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+	}
+}
