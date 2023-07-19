5461230c8cb5134365948ab055f9345d54f9374c
Houdae013 <135075472+Houdae013@users.noreply.github.com>
2023-07-19 02:41:35 +0200

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,40 +9,40 @@
 void times_table(void)
 {
 	int i, j;
-
+	
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
-			{
+		{
 			if (i == 0)
-			{_putchar('0');
-			 if (j < 9)
-			 {
-			 _putchar (',');	 
+		{
+			_putchar('0');
+		 if (j < 9)
+		 {
+			 _putchar (',');
 			 _putchar(32);
-				 _putchar (32);
-			} }
-			else
+			 _putchar (32);
+		 } }
+		else
 			{
 			if (((i * j) / 10) == 0)
-			{_putchar (j * i + '0');
-			if (j < 9)
-			 {
-			 _putchar (',');	 
-			 _putchar(32);
-				 _putchar (32);
-			} 
+			{
+				_putchar (j * i + '0');
+				if (j < 9)
+			{
+				_putchar (',');
+				_putchar(32);
+				_putchar (32);
+			}
 			}
 			else
-			{_putchar (((i * j) / 10) + '0');
-				_putchar (((i * j) % 10) + '0'); 
-			if (j < 9)
 			{
-
-			_putchar (',');
-			_putchar(32); }
-			
-		} } }
-		_putchar('\n');
-	}
-}
+				_putchar (((i * j) / 10) + '0');
+			 _putchar (((i * j) % 10) + '0');
+			 if (j < 9)
+			 {
+				 _putchar (',');
+				 _putchar(32); }
+			} } }
+	 _putchar('\n');
+	} }
