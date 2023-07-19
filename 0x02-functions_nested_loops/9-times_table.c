1bbdc4538b74af4500376462b0fe11143210a089
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-18 17:01:28 -0700

    ut

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * times_table - checks for lower
+ *
+ * On error: return 0
+ * Return: 1 if success
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (((i * j) / 10) == 0)
+			{_putchar (j + '0'); }
+			else
+			{_putchar (((i * j) / 10) + '0');
+				_putchar (((i * j) % 10) + '0');
+			}
+			_putchar (',');
+			_putchar(32);
+		}
+		_putchar('\n');
+	}
+}
