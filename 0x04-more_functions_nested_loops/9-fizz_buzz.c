1590ec36af740c1623db3549d4ce8c2e7c958b6e
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-20 16:16:32 -0700

    o

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
 			printf("Fizz");
@@ -19,6 +19,8 @@ int main(void)
 			printf("FizzBuzz");
 		else
 			printf("%d", i);
-		printf("\t");
+		printf(" ");
 	}
+	printf("\n");
+	return (0);
 }
