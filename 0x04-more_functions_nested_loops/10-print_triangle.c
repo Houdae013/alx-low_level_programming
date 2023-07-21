4a105c4a8988eb323bd0ea57511e5e4c3e8ea5e0
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-20 17:19:27 -0700

    o

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle- print triangle
+ * @size: int
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <=  size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j < size - i)
+			{
+				_putchar(32);
+			}
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
