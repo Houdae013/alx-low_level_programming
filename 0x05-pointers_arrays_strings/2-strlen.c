2188ccba0b2eae0e03770e5c310aa8be6326e58c
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-24 11:15:45 -0700

    o

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen- lenght of a string
+ * @s: char
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	*s = s[0];
+	while (*s <= "/0")
+	{
+		*s++;
+		i++;
+	}
+	return (i);
+}
