da8e1ea1bea3d4436d58cdc6844d816ccde8ecf8
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-28 10:06:30 -0700

    task 3.strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,6 @@
 int _strcmp(char *s1, char *s2)
 {
 	int diff = 0;
-	int i = 0;
 
 
 	while (*s1 && *s2)
@@ -23,10 +22,10 @@ int _strcmp(char *s1, char *s2)
 		}
 		else
 		{
-			diff = s1[i + 1] - s2[i + 1];
+			diff = *s1 - *s2;
 			return (diff);
 		}
 
 	}
-	return (0);
+	return (diff);
 }
