d8bfa6c416708d1c6287a46aff7214ccd457c748
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-28 11:27:04 -0700

    task 5 AMWAYS LOOK UP

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,13 +2,14 @@
 
 /**
  * *string_toupper- changes all lowercase letters to uppercase
+ * @str: char
  * Return: char
  */
 
 char *string_toupper(char *str)
 {
 	char *p = str;
-	
+
 	while (*p)
 	{
 		if (*p <= 122 && *p >= 97)
