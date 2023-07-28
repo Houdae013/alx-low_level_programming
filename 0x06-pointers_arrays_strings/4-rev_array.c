1cbf8ad6f3f31016a7103725d353fd09c43c1492
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-28 10:29:56 -0700

    task 4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array- reverses the content of an arrayof integers
+ * @a: int
+ * @n: int
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, p;
+	int *end = a[n - 1];
+	int *start = a;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		p = *end;
+		*end = *start;
+		*start = p;
+	}
+}
+
