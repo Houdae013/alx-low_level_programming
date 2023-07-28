ed23e7b0a565cc59e9d77805e86b66b1d237c48b
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-07-28 09:42:56 -0700

    task 3.strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp- compares two strings
+ * @s1: char
+ * @s2: char
+ * Return: int
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int diff;
+	while (*s1 && *s2)
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+			return (diff = *s1 - *s2);
+	}
+}
