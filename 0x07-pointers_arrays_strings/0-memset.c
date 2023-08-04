216dd11d33dc05d4492d7ccfda9138175141ad13
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-08-04 12:03:13 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,14 +8,14 @@
  */
 
 char *_memset(char *s, char b, unsigned int n)
-{
-	int i;
+{	
+	unsigned int i;
 
 
 	for (i = 0; i < n; i++)
 	{
 		*s = b;
-		b++;
+		s++;
 	}
-	return (*s);
+	return (s);
 }	
