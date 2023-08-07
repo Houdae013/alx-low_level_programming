16af58213d25d929122b02d5da3cda91453e6f11
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-08-07 16:04:55 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main- entry point
+ * @argc: nbr
+ * @argv: char
+ * Return: hh
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
