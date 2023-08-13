6f30b48b53b9a3222849cc7d9f468ceddf3a8592
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-08-13 16:38:28 -0700

    advanced task 1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,33 +15,31 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (ptr == NULL)
 		return (malloc(new_size));
-	else
-	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		else if (new_size == old_size)
-			return (ptr);
-		ns = malloc(new_size);
-		if (ns == NULL)
-			return (NULL);
 
-		if (new_size > old_size)
-		{
-			for (i = 0; i < old_size; i++)
-			{
-				ns[i] = os[i];
-			}
-			free(ptr);
-		}
-		else if (new_size < old_size)
-		{
-			for (i = 0; new_size > i; i++)
-				ns[i] = os[i];
-			free(ptr);
-		}
-		return (ns);
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
 	}
+	else if (new_size == old_size)
+		return (ptr);
+	ns = malloc(new_size);
+	if (ns == NULL)
+		return (NULL);
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+		{
+			ns[i] = os[i];
+		}
+		free(ptr);
+	}
+	else if (new_size < old_size)
+	{
+		for (i = 0; new_size > i; i++)
+			ns[i] = os[i];
+		free(ptr);
+	}
+	return (ns);
 }
