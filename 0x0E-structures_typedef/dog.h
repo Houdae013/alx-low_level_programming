d508b90d52aaedbc4b77acbc6261e896a2d23645
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-08-15 09:30:50 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog- struscture
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
