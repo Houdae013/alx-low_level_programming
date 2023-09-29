4dbcee4082d560981275b96757778294f6e8c22b
Houdae013 <houdaelyoussoufi5@gmail.com>
2023-09-29 21:29:36 +0100

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_binary - prints the binary representation of a number
+* @n: unsigned long int
+* Return: void
+*/
+
+void print_binary(unsigned long int n)
+{
+	if (n == 1 || n == 0)
+	{
+		_putchar(n + '0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
